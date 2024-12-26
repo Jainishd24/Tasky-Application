7e6c1283623805316f5f509a432c66fa3c3e8250
Jainish Devpura <143697297+Jainishd24@users.noreply.github.com>
2024-12-26 05:53:23 +0000

    Demoserver

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const port = 8081;
 http .createServer((req,res)=>{
     res.writeHead(200,{"content-type":"text/html"});
-    res.write("<h2>The server is started</h2>");
+    res.write("<h2>The server is started :)</h2>");
     res.end();
 })
 .listen(port,()=>{
